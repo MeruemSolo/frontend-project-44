a50dfb3c5e2e3d74024d641e908be1a05f720d77
MeruemSolo <arturmeruem@gmail.com>
2022-12-19 13:14:19 +0300

    brain-prime

--- bin/utils.js
+++ bin/utils.js
@@ -2,5 +2,13 @@ const random = (min, max) => {
     const rand = Math.floor(Math.random() * (max - min + 1)) + min;
     return Math.floor(rand);
   };
-  
-  export default random;
+
+const isPrime = (randomNumber) => {
+    if (randomNumber === 2) return true;
+    if (randomNumber <= 1) return false;
+    if (randomNumber % 2 === 0) {
+     return false;
+      }
+    return true;
+   }
+   export default { random, isPrime };
