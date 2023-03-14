8d2699a64983e95001ade5fe19413dc6134b0e56
MeruemSolo <arturmeruem@gmail.com>
2023-03-14 11:03:22 +0300

    upd all

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+
 import readlineSync from 'readline-sync'
+
 export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const random = (min, max) => {
   const rand = Math.floor(Math.random() * (max - min + 1)) + min
-  return Math.floor(rand)
+  return rand
 }
 
 export default random
