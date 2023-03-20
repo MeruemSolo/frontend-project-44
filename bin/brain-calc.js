ba292b78224fec32f202b652a85b9387e69ab9db
MeruemSolo <arturmeruem@gmail.com>
2023-03-20 13:11:27 +0300

    brain-calc upd

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 const brainCalc = () => {
   console.log('Welcome to the Brain Games!');
@@ -16,22 +16,22 @@ const brainCalc = () => {
     const userAnswer = readlineSync.question('Your answer: ');
     const calcArithmeticOperation = (symbol, firstValue, secondValue) => {
       switch (symbol) {
-        case '+': return firstValue + secondValue
-        case '-': return firstValue - secondValue
-        case '*': return firstValue * secondValue
-        default: return null
+        case '+': return firstValue + secondValue;
+        case '-': return firstValue - secondValue;
+        case '*': return firstValue * secondValue;
+        default: return null;
       }
     };
     const correctAnswer = String(calcArithmeticOperation(randomOperator, randomNumber1, randomNumber2));
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`)
-      break
-    };
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      break;
+    }
     if (i === 2) {
-      console.log(`Congratulations, ${name}!`)
-    };
-  };
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
 };
 export default brainCalc();
