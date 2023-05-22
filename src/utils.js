103bef0da0507f233f2b441bf3abe8ef221981f9
MeruemSolo <arturmeruem@gmail.com>
2023-05-22 13:50:04 +0300

    fixes

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,2 @@
-export const getRandomInRange = (min, max) => {
-  return Math.floor(min + Math.random() * (max - min + 1));
-};
+const getRandomInRange = (min, max) => { return Math.floor(min + Math.random() * (max - min + 1)) };
 export default getRandomInRange;
