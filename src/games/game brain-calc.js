846dcd7f5a6b820dce5ca363721270562f606724
MeruemSolo <arturmeruem@gmail.com>
2023-09-11 13:58:08 +0300

    crlf > lf

--- src/games/game brain-calc.js
+++ src/games/game brain-calc.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const brainCalc = () => {
+const brainCalc = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
